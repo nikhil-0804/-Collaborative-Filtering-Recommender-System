520af1815382a9e4554fbfb0774dfa7cf2d5a21a
nikhil 0804 <nikhilrana6191@gmail.com>
2025-03-30 15:16:13 +0530

    load precomputed data

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import pickle
 import numpy as np
 import requests
 from collections import defaultdict
+
+# Load precomputed data
+popular_df = pickle.load(open('popular.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
